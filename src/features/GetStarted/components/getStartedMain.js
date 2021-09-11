51b255c33ea6baed01bb67cd9fcfae64a327282a
Breno Capaz <brenocapaz@gmail.com>
2021-09-11 15:42:35 -0700

    video training

--- src/features/GetStarted/components/getStartedMain.js
+++ src/features/GetStarted/components/getStartedMain.js
@@ -1,51 +1,69 @@
-import { Button, Container, Grid, Typography, useMediaQuery, useTheme } from '@material-ui/core';
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import getStartedImage from '../../../images/get-started.png';
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import getStartedImage from "../../../images/get-started.png";
 
-
-const GetStartedMain = ({onStart, onSee}) => {
-    const theme = useTheme();
-    const aboveSmall = useMediaQuery(theme.breakpoints.up('sm'));
-    return (
-        <Container>
-            <Grid container justify="center" spacing={6}>
-                <Grid item container direction="column"  justify="space-around" md={6} sm={12}>
-                    <Grid item>
-                        <Typography variant="h4" gutterBottom><FormattedMessage id="get-started.more-than-software" /></Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h5" ><FormattedMessage id="get-started.we-are-team" /></Typography>
-                    </Grid>
-                    <Grid item container justify="center" spacing={4}>
-                        <Grid item>
-                            <Button variant="contained" color="primary" style={{ textTransform: 'none', backgroundColor: 'white', color: 'black', height: '3rem', width: '15rem' }} onClick={onStart}>
-                                <Typography variant="h6">
-                                    <FormattedMessage id="get-started" />
-                                </Typography>
-                            </Button>
-                        </Grid>
-
-                        <Grid item>
-                            <Button variant="outlined" style={{ textTransform: 'none', color: 'white', border: '1px solid white', height: '3rem', width: '15rem' }} startIcon={<PlayCircleOutlineIcon fontSize="large" />} onClick={onSee}>
-                                <Typography variant="body1" align="center">
-                                    <FormattedMessage id="get-started.see-platform" />
-                                </Typography>
-                            </Button>
-                        </Grid>
-
-                    </Grid>
-                </Grid>
-
-                {aboveSmall && <Grid item xs={5}>
-                    <img src={getStartedImage} alt="hajonsoft software" width="300px" />
-                </Grid>}
-
+const GetStartedMain = ({ onStart }) => {
+  const theme = useTheme();
+  const aboveSmall = useMediaQuery(theme.breakpoints.up("sm"));
+  return (
+    <Container>
+      <Grid container justify="center" spacing={6}>
+        <Grid
+          item
+          container
+          direction="column"
+          justify="space-around"
+          md={6}
+          sm={12}
+        >
+          <Grid item>
+            <Typography variant="h4" gutterBottom>
+              <FormattedMessage id="get-started.more-than-software" />
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">
+              <FormattedMessage id="get-started.we-are-team" />
+            </Typography>
+          </Grid>
+          <Grid item container justify="center" spacing={4}>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{
+                  textTransform: "none",
+                  backgroundColor: "white",
+                  color: "black",
+                  height: "3rem",
+                  width: "15rem",
+                }}
+                onClick={onStart}
+              >
+                <Typography variant="h6">
+                  <FormattedMessage id="get-started" />
+                </Typography>
+              </Button>
             </Grid>
-        </Container>
+          </Grid>
+        </Grid>
 
-    )
-}
+        {aboveSmall && (
+          <Grid item xs={5}>
+            <img src={getStartedImage} alt="hajonsoft software" width="300px" />
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
+};
 
-export default GetStartedMain
+export default GetStartedMain;
--- src/features/GetStarted/index.js
+++ src/features/GetStarted/index.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
-import GetStartedLearn from './components/getStartedLearn';
-import GetStartedMain from './components/getStartedMain';
-import GetStartedSee from './components/getStartedSee';
-import {analytics} from '../../analytics';
+import React from "react";
+import { analytics } from "../../analytics";
+import GetStartedMain from "./components/getStartedMain";
 
 const GetStarted = () => {
-    const [state, setState] = useState(0);
+  const handleStart = () => {
+    analytics.logEvent("get-started");
+    window.location.href = 'https://hajonsoft.talentlms.com/catalog/info/id:125,cms_featured_course:1'; 
+  };
 
-    const handleStart = () => {
-        setState(1);
-        analytics.logEvent('get-started');
-    }
-    const handleSeePlatform = () => {
-        setState(2);
-        analytics.logEvent('see-platform');
-    }
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(90deg, rgba(56,134,176,1) 0%, rgba(71,161,177,1) 100%)",
+        color: "white",
+        height: "21rem",
+        marginTop: "1rem",
+      }}
+    >
+      <GetStartedMain onStart={handleStart} />
+    </div>
+  );
+};
 
-    return (
-        <div style={{ background: 'linear-gradient(90deg, rgba(56,134,176,1) 0%, rgba(71,161,177,1) 100%)', color: 'white', height: '21rem', marginTop: '1rem' }}>
-            {state === 0 && <GetStartedMain onStart={handleStart} onSee={handleSeePlatform} />}
-            {state === 1 && <GetStartedLearn />}
-            {state === 2 && <GetStartedSee />}
-        </div>
-
-    )
-}
-
-export default GetStarted
+export default GetStarted;
