1ab92ca26089710b2bee0a96f5d26b32d6b829d7
aali-bright <ayman@brightlive.com>
2022-03-10 05:04:07 -0800

    footer and timezone

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, Typography } from '@material-ui/core'
+import { Button, Container, Grid, Link, Typography } from '@material-ui/core'
 import React from 'react'
 import logo from '../../images/logo.png';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -9,7 +9,8 @@ import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 import footerBg from '../../images/footer_bg.svg'
 import owl from '../../images/owl.svg'
-import {colors} from '../../util/colors'
+import { colors } from '../../util/colors'
+import moment from 'moment-timezone';
 
 const facebookUrl = 'https://www.facebook.com/Hajonsoft-108504544787000';
 
@@ -18,21 +19,26 @@ const Footer = () => {
     return (
         <div style={{ backgroundImage: `url(${footerBg})` }}>
             <Container>
-                <Grid container justify="center" spacing={2} alignItems="stretch">
-                    <Grid item sm={12} md={3} lg container justify="center" spacing={2}>
+                <Grid container alignItems="stretch">
+                    <Grid item sm={12} md={3} lg container >
                         <Grid item sm={12} md={12} lg={12}>
                             <img src={logo} alt="logo" width="128" />
                         </Grid>
                         <Grid item sm={12} md={12} lg={12}>
-                            <Typography variant="body2">
+                            <Typography variant="caption">
                                 hajonsoft@gmail.com
                             </Typography>
                         </Grid>
                         <Grid item sm={12} md={12} lg={12}>
-                            <Typography variant="body2"><FormattedMessage id="header.telephone" /></Typography>
+                            <Typography variant="caption" align='center'>
+                                {moment.tz.guess()}
+                            </Typography>
+                        </Grid>
+                        <Grid item sm={12} md={12} lg={12}>
+                            <Typography variant="caption"><FormattedMessage id="header.telephone" /></Typography>
                         </Grid>
                     </Grid>
-                    <Grid item sm={12} md={2} lg container spacing={2} alignContent="flex-start">
+                    <Grid item sm={12} md={2} lg container spacing={1} alignContent="flex-start">
                         <Grid item xs={12}>
                             <Typography variant="button"><FormattedMessage id="footer.company" /></Typography>
                             <div style={{ border: '2px solid rgb(57,63,82)', width: '90%' }}></div>
@@ -47,7 +53,7 @@ const Footer = () => {
                             <FormattedMessage id="find" />
                         </Grid>
                     </Grid>
-                    <Grid item sm={12} md={2} lg container spacing={2} alignContent="flex-start">
+                    <Grid item sm={12} md={2} lg container spacing={1} alignContent="flex-start">
                         <Grid item xs={12}>
                             <Typography variant="button"><FormattedMessage id="footer.resources" /></Typography>
                             <div style={{ border: '2px solid rgb(57,63,82)', width: '90%' }}></div>
@@ -56,16 +62,29 @@ const Footer = () => {
                         <Grid item xs={12}><FormattedMessage id="footer.common-questions" /></Grid>
                         <Grid item xs={12}><FormattedMessage id="footer.insights" /></Grid>
                     </Grid>
-                    <Grid item sm={12} md={2} lg container spacing={2} alignContent="flex-start">
+                    <Grid item sm={12} md={2} lg container spacing={1} alignContent="flex-start">
                         <Grid item xs={12}>
                             <Typography variant="button"><FormattedMessage id="footer.our-product" /></Typography>
                             <div style={{ border: '2px solid rgb(57,63,82)', width: '90%' }}></div>
                         </Grid>
-                        <Grid item xs={12}><FormattedMessage id="footer.desktop" /></Grid>
-                        <Grid item xs={12}><FormattedMessage id="web" /></Grid>
-                        <Grid item xs={12}><FormattedMessage id="header.pricing" /></Grid>
+                        <Grid item xs={12}>
+                            <Link href="https://github.com/hajonsoft/hajonsoft-hawk" target='_blank' variant="caption">
+                                <FormattedMessage id="footer.Hawk" />
+                            </Link>
+
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Link href="https://github.com/hajonsoft/hajonsoft-eagle" target='_blank' variant="caption">
+                                <FormattedMessage id="footer.Eagle" />
+                            </Link>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Link href="https://github.com/hajonsoft/hajonsoft-tiger" target='_blank' variant="caption">
+                                <FormattedMessage id="footer.Nest" />
+                            </Link>
+                        </Grid>
                     </Grid>
-                    <Grid item sm={12} md={2} lg container spacing={2} alignContent="flex-start">
+                    <Grid item sm={12} md={2} lg container spacing={1} alignContent="flex-start">
                         <Grid item xs={12}>
                             <Typography variant="button"><FormattedMessage id="get-the-app" /></Typography>
                             <div style={{ border: '2px solid rgb(57,63,82)', width: '90%' }}></div>
@@ -77,9 +96,9 @@ const Footer = () => {
                 </Grid>
             </Container>
             <div style={{ width: '100%', height: '4rem', backgroundColor: 'rgb(57,63,82)', marginTop: '1rem', marginBottom: '1rem', display: 'flex', alignItems: 'center', color: 'white' }}>
-                <Grid container justify="space-between" spacing={2} alignItems="center">
+                <Grid container justify="space-between" spacing={1} alignItems="center">
                     <Grid item style={{ marginLeft: '2rem' }}>
-                        <Grid container spacing={2} alignItems="center">
+                        <Grid container spacing={1} alignItems="center">
                             <Grid item>
                                 © 2020 HAJonSoft Owl
                             </Grid>
@@ -97,7 +116,7 @@ const Footer = () => {
                     <Grid item></Grid>
                     <Grid item>
                         <a href={facebookUrl} target="_blank" alt="facebook" rel="noreferrer">
-                            <FacebookIcon style={{color: colors.white}}/>
+                            <FacebookIcon style={{ color: colors.white }} />
                         </a>
                         <LinkedInIcon />
                         <TwitterIcon />
